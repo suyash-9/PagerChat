759a21c291b86d06df67219a762523e4fa830b39
Suyash Sinha <sinha.suyash.ss@gmail.com>
2021-09-18 17:10:07 +0530

    Second Commit

--- app/src/main/java/com/example/pagerchat/PeopleFragment.kt
+++ app/src/main/java/com/example/pagerchat/PeopleFragment.kt
@@ -1,7 +1,97 @@
 package com.example.pagerchat
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.paging.PagedList
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.firebase.ui.firestore.paging.FirestorePagingAdapter
+import com.firebase.ui.firestore.paging.FirestorePagingOptions
+import com.firebase.ui.firestore.paging.LoadingState
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import kotlinx.android.synthetic.main.fragment_chats.*
+import java.lang.Exception
 
 class PeopleFragment:Fragment() {
+    lateinit var mAdapter : FirestorePagingAdapter<User,UserViewHolder>
+    val auth by lazy {
+        FirebaseAuth.getInstance()
+    }
+    val database by lazy {
+        FirebaseFirestore.getInstance().collection("users").orderBy("name", Query.Direction.DESCENDING)
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        //viewManager = LinearLayoutManager(requireContext())
+        setupAdapter()
+        return inflater.inflate(R.layout.fragment_chats, container, false)
+    }
+
+    private fun setupAdapter() {
+        val config=PagedList.Config.Builder()
+            .setPrefetchDistance(2)
+            .setPageSize(10)
+            .setEnablePlaceholders(false)
+            .build()
+
+        val options=FirestorePagingOptions.Builder<User>()
+            .setLifecycleOwner(viewLifecycleOwner)
+            .setQuery(database,config,User::class.java)
+            .build()
+
+        mAdapter= object : FirestorePagingAdapter<User,UserViewHolder>(options){
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+                val view=layoutInflater.inflate(R.layout.list_item,parent,false)
+                return UserViewHolder(view)
+            }
+
+            override fun onBindViewHolder(holder: UserViewHolder, p1: Int, model: User) {
+                holder.bind(user = model)
+
+            }
+
+            override fun onLoadingStateChanged(state: LoadingState) {
+                super.onLoadingStateChanged(state)
+                when (state) {
+                    LoadingState.LOADING_INITIAL -> {
+                    }
+
+                    LoadingState.LOADING_MORE -> {
+                    }
+
+                    LoadingState.LOADED -> {
+                    }
+
+                    LoadingState.ERROR -> {
+                        Toast.makeText(
+                            requireContext(),
+                            "Error Occurred!",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+
+            }
+
+            override fun onError(e: Exception) {
+                super.onError(e)
+            }
+
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerView.apply {
+            layoutManager=LinearLayoutManager(requireContext())
+        }
+    }
 
 }
---
+++ app/src/main/java/com/example/pagerchat/UserViewHolder.kt
@@ -0,0 +1,29 @@
+package com.example.pagerchat
+
+
+import android.view.View
+
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+
+import kotlinx.android.synthetic.main.list_item.view.*
+
+class UserViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
+
+    fun bind(user:User)= with(itemView){
+        countTv.isVisible=false
+        timeTv.isVisible=false
+
+        titleTv.text=user.name
+        subTitleTv.text=user.status
+
+        Picasso.get()
+            .load(user.thumbImage)
+            .placeholder(R.drawable.defaultavatar)
+            .error(R.drawable.defaultavatar)
+            .into(userImgView)
+    }
+
+
+}
